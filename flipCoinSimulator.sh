922f77f2c89e5b954052c7f61c2c9f782f39a543
NitinJ1996 <nitin1996j@gmail.com>
2020-07-10 16:26:22 +0530

    {Nitin} UseCase 3 - Multiple coin flip checking for tie Update

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,15 +1,28 @@
 #!/bin/bash -x
 
-read -p "Enter the number of times to flip a coin: " num
-count=0
-while [ $count -lt $num ]
+#variables
+headwin_count=0
+tailwin_count=0
+
+#check for multiple coin simulations
+while [[ $headwin_count -le 21 || $tailwin_count -le 21 ]]
 do
 	coinflip=$((RANDOM%2))
 	if [ $coinflip -eq 1 ]
 	then 
-			echo "HEADS"
+			((headwin_count++)) 
 	else
-			echo "TAILS"
+			((tailwin_count++))
 	fi
-	((count++))
 done
+
+#Checking if its a tie or win
+if [ $headwin_count -eq $tailwin_count ]
+then
+		echo "TIE"
+elif [ $headwin_count -gt $tailwin_count ]
+then
+		echo "HEAD wins by $((headwin_count-tailwin_count))"
+else
+		echo "TAIL wins by $(($tailwin_count-$headwin_count))"
+fi
