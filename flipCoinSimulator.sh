3cc9812e5e5a9797584375fdac0b442346d5798e
NitinJ1996 <nitin1996j@gmail.com>
2020-07-10 17:01:28 +0530

    {Nitin} UseCase 4 - Coin flip tie condition Update

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 #variables
 headwin_count=0
 tailwin_count=0
+valid=true
 
-#check for multiple coin simulations
-while [[ $headwin_count -le 21 || $tailwin_count -le 21 ]]
-do
+#function for coin simulation
+function flipCoinSimulation(){
 	coinflip=$((RANDOM%2))
 	if [ $coinflip -eq 1 ]
 	then 
@@ -14,15 +14,32 @@ do
 	else
 			((tailwin_count++))
 	fi
+}
+
+#multiple coin simuations till either reaches 21
+while [[ $headwin_count -le 21 || $tailwin_count -le 21 ]]
+do
+		flipCoinSimulation
 done
 
-#Checking if its a tie or win
+#Checking if its a tie
 if [ $headwin_count -eq $tailwin_count ]
 then
 		echo "TIE"
-elif [ $headwin_count -gt $tailwin_count ]
+		while [ $valid ]
+		do
+				flipCoinSimulation
+				if [[ $(($headwin_count-$tailwin_count)) -eq 2 || $(($tailwin_count-$headwin_count)) -eq 2 ]]
+				then
+						break
+				fi
+		done
+fi
+
+#check if head or tail won and by how much
+if [ $headwin_count -gt $tailwin_count ]
 then
-		echo "HEAD wins by $((headwin_count-tailwin_count))"
+		echo "HEAD wins by $(($headwin_count-$tailwin_count))" 
 else
 		echo "TAIL wins by $(($tailwin_count-$headwin_count))"
 fi
