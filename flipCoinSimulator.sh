55e823367622b8a58af93aa4cd1308af7b989fcd
NitinJ1996 <nitin1996j@gmail.com>
2020-07-10 16:08:56 +0530

    {Nitin} UseCase 2 - Multiple Coin Flip Simmulation Update

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 
-coinflip=$((RANDOM%2))
-if [ $coinflip -eq 1 ]
-then 
-		echo "HEADS"
-else
-		echo "TAILS"
-fi
+read -p "Enter the number of times to flip a coin: " num
+count=0
+while [ $count -lt $num ]
+do
+	coinflip=$((RANDOM%2))
+	if [ $coinflip -eq 1 ]
+	then 
+			echo "HEADS"
+	else
+			echo "TAILS"
+	fi
+	((count++))
+done
